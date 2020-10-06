28e4a065ffed5eca2b93623b57a6cf34595f3c88
Wooyang Son <sonwy102@gmail.com>
2020-10-06 00:20:44 +0000

    Completed wordcount.py

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,18 @@
 # put your code here.
+def count_words(filename):
+
+    text = open(filename)
+    word_count = {}
+    
+    for line in text:
+
+        line = line.rstrip()
+        words = line.split(" ")
+        
+        #iterate over words and add into word_count dict
+        for word in words:
+
+            word = word.rstrip("?!.,")
+            word_count[word] = word_count.get(word, 0) + 1
+    
+    print(word_count)
